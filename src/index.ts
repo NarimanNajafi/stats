11835e555c7f63b73e5816f0c2c06da1966c82c6
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-10-08 01:11:16 +0330

    index updated

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('/n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read;
 
 enum MatchResult {
   HomeWin = 'H',
@@ -15,17 +9,9 @@ enum MatchResult {
   Draw = 'D',
 }
 
-const printMatchResult = (): MatchResult => {
-  if (match[5] === 'H') {
-    return MatchResult.HomeWin;
-  }
-
-  return MatchResult.AwayWin;
-};
-
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
