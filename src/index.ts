31c22af20d1cb392e794ed28002a62ed978320bd
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2023-10-08 00:47:49 +0330

    src added!

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('/n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+const printMatchResult = (): MatchResult => {
+  if (match[5] === 'H') {
+    return MatchResult.HomeWin;
+  }
+
+  return MatchResult.AwayWin;
+};
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United wons${manUnitedWins} games`);
